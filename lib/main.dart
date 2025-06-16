9aefb640a9da51255efa0d5ef9ae3be62d1148dc
junyoungkim <juny3738@gmail.com>
2025-06-16 23:16:12 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
-  await startServer();
-
-  // runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
