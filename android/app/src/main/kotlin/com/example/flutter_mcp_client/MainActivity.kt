9436d4fdbbf9fff1a656a5c9d41960c035d927cd
junyoungkim <juny3738@gmail.com>
2025-06-16 22:47:10 +0900

    test

---
+++ android/app/src/main/kotlin/com/example/flutter_mcp_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_mcp_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_common/constants/index.dart';
+import 'package:flutter_common/state/app/app_bloc.dart';
+import 'package:flutter_common/state/app_config/app_config_bloc.dart';
+import 'package:flutter_common/state/user/user_bloc.dart';
+import 'package:flutter_common/state/user/user_event.dart';
+import 'package:flutter_common/widgets/app/app_screen.dart';
+import 'package:flutter_common/widgets/layout/chat_screen.dart';
+import 'package:flutter_common/widgets/layout/setting_screen_layout.dart';
+import 'package:flutter_common/widgets/layout/notice_screen_layout.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  AppBloc get appBloc => context.read<AppBloc>();
+  AppConfigBloc get appConfigBloc => context.read<AppConfigBloc>();
+  UserBloc get userBloc => context.read<UserBloc>();
+  @override
+  void initState() {
+    super.initState();
+    userBloc.add(const UserEvent.initialize());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScreen(
+      screens: [
+        ChatScreen(),
+        NoticeScreenLayout(groupName: 'parking-zone-code-02782'),
+        SettingScreenLayout(appKey: AppKeys.caughtSmoking),
+      ],
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.chat), label: '채팅'),
+        BottomNavigationBarItem(icon: Icon(Icons.forum), label: '커뮤니티'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: '설정'),
+      ],
+      bloc: appBloc,
+    );
+  }
+}
