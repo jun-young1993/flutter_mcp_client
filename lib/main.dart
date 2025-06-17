8bcbf13884dc0b213187f8b6f49a8c40da8303be
junyoungkim <juny3738@gmail.com>
2025-06-17 22:43:40 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,23 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_common/constants/juny_constants.dart';
 import 'package:flutter_common/network/dio_client.dart';
 import 'package:flutter_common/providers/common_provider.dart';
+import 'package:flutter_common/repositories/llm_client_repository.dart';
 import 'package:flutter_common/state/chat/chat_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:flutter_mcp_client/ui/main_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  final prefs = await SharedPreferences.getInstance();
 
   runApp(
     CommonProvider(
       dioClient: DioClient(),
       appKey: AppKeys.caughtSmoking,
+      sharedPreferences: prefs,
       child: MyApp(child: MainScreen()),
     ),
   );
